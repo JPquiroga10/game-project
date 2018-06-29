13f40a2f16f8ca0642355129710829ba7c5f7e6b
John-Paul Quiroga <Quiroga_jp@yahoo.com>
2018-06-28 23:12:46 -0400

    jumper

--- doodle-canvas.js
+++ doodle-canvas.js
@@ -39,7 +39,7 @@ window.onload = function() {
     }
   };
 
-  // CONTSTRUCTOR FUNCTION TO DRAW OBJECTS (chewy,platform) 
+  // CONTSTRUCTOR FUNCTION TO DRAW OBJECTS (chewy,platform)
   function Component(width, height, image, x, y, type) {
     this.width = width;
     this.height = height;
@@ -50,8 +50,7 @@ window.onload = function() {
     this.y = y;
     this.speedY = 0;
     this.speedX = 0;
-    
-    
+
     this.image = new Image();
     this.update = function() {
       ctx = myGameArea.context;
@@ -62,110 +61,71 @@ window.onload = function() {
         this.image.src = "images/platform.png";
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
       }
-    }
+    };
     this.newPos = function() {
-        this.gravitySpeed += this.gravity;
-        this.x += this.speedX;
-        this.y += this.speedY + this.gravitySpeed;
-        this.hitBottom();
-    }
+      this.gravitySpeed += this.gravity;
+      this.x += this.speedX;
+      this.y += this.speedY + this.gravitySpeed;
+      this.hitBottom();
+    };
     this.hitBottom = function() {
-        var rockbottom = myGameArea.canvas.height - this.height;
-        if (this.y > rockbottom) {
-            this.y = rockbottom;
-            this.gravitySpeed = 0;
-        }
-        
-  
+      var rockbottom = myGameArea.canvas.height - this.height;
+      if (this.y > rockbottom) {
+        this.y = rockbottom;
+        this.gravitySpeed = -01;
+      }
     };
-    
-    this.left = function() {
-      return this.x;
-    };
-    this.right = function() {
-      return this.x + this.width -35;
-    };
-    this.top = function() {
-      return this.y;
-    };
-    this.bottom = function() {
-      return this.y + this.height -5;
-    };
+
+   
     this.crashWith = function(obstacle) {
-      return !(
-        player.bottom() < obstacle.top() ||
-        player.top() > obstacle.bottom() ||
-        player.right() < obstacle.left() ||
-        player.left() > obstacle.right()
+      return (
+        this.y + this.height < obstacle.y &&
+        this.x + this.width > obstacle.x &&
+        this.x < obstacle.x + obstacle.width &&
+        obstacle.y - this.y - this.height < 40
       );
 
-    this.platformStep = function () {
-      if(player.bottom >= obstacle.top) {
-        player.x = obstacle.posX;
-        this.hitBottom()
-      }
-    }
+      // this.platformStep = function(obstacle) {
+      //   if (player.bottom >= obstacle.top) {
+      //     player.x = obstacle.posX;
+      //     this.hitBottom();
+      //   }
+      // };
+      // this.platformStep();
     };
   }
 
-  // function updateGameArea() {
-  //   for (i = 0; i < myGameArea.myObstacles.length; i += 1) {
-  //     if (player.crashWith(myGameArea.myObstacles[i])) {
-  //       myGameArea.stop();
-  //       startGame();
-  //       return;
-  //     }
-  //   }
-  //   myGameArea.clear();
-  //   myGameArea.backgroud();
-  //   drawObstacles();
-  //   myGameArea.frames += 4;
-  //   for (i = 0; i < myGameArea.myObstacles.length; i += 1) {
-  //     myGameArea.myObstacles[i].y += 1;
-  //     myGameArea.myObstacles[i].update();
-  //   }
-  //   player.update();
-  //   player.newPos();
-  //   myGameArea.reqAnimation = window.requestAnimationFrame(updateGameArea);
-
   
-  // }
 
   // FUNCTION THAT DRAWS new platforms
   function drawObstacles() {
     if (myGameArea.frames % 110 === 0) {
       minWidth = (myGameArea.canvas.width - 5) * 0.09;
       maxWidth = (myGameArea.canvas.width - 5) * 0.2;
-      width = minWidth + Math.floor(Math.random() * ((maxWidth-20) - minWidth));
+      width = minWidth + Math.floor(Math.random() * (maxWidth - 20 - minWidth));
       posX =
         40 + Math.floor(Math.random() * (myGameArea.canvas.width - 80 - width));
-      myGameArea.myObstacles.push(new Component(width, 20, "images/platform.png", posX, 0));
+      myGameArea.myObstacles.push(
+        new Component(width, 20, "images/platform.png", posX, 0)
+      );
     }
-    
   }
-  
+
   // Key Down function
   document.onkeydown = function(e) {
-    if (e.keyCode == 39 && player.x < myGameArea.canvas.width -60 ) {
+    if (e.keyCode == 39 && player.x < myGameArea.canvas.width - 60) {
       player.x += 30;
     }
     if (e.keyCode == 37 && player.x > 0) {
       player.x -= 30;
     }
-    if (e.keyCode == 38 ) {
+    if (e.keyCode == 38) {
       player.y -= 170;
-      this.platformStep();
     }
   };
 
   function updateGameArea() {
-    for (i = 0; i < myGameArea.myObstacles.length; i += 1) {
-      if (player.crashWith(myGameArea.myObstacles[i])) {
-        myGameArea.stop();
-        startGame();
-        return;
-      }
-    }
+    var withGravity = true;
     myGameArea.clear();
     myGameArea.backgroud();
     drawObstacles();
@@ -174,11 +134,25 @@ window.onload = function() {
       myGameArea.myObstacles[i].y += 1;
       myGameArea.myObstacles[i].update();
     }
+
+    for (i = 0; i < myGameArea.myObstacles.length; i += 1) {
+      if (player.crashWith(myGameArea.myObstacles[i])) {
+        player.y = myGameArea.myObstacles[i].y - player.height;
+        withGravity = false;
+        
+      }
+    }
+    if (withGravity) {
+      player.newPos();
+    }
+
     player.update();
-    player.newPos();
+
+    //win or lose function
+    
     myGameArea.reqAnimation = window.requestAnimationFrame(updateGameArea);
   }
- 
-
 };
+
+
 
